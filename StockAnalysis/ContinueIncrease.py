901f5a7a6855a44a5cd71b98fdb9b672d7f3e85b
StupidCodeGenerator <c@thebgame.com>
2015-11-11 17:19:35 +0800

    update

--- StockAnalysis/ContinueIncrease.py
+++ StockAnalysis/ContinueIncrease.py
@@ -10,53 +10,68 @@ import os
 s = os.sep
 root = sys.argv[1]
 
-data = []
+def ContinueRaiseOfDays(data, today):
+	if today == 0:
+		return 0
+	# TODO : count the raises
 
+# It will return the num of Raise and Fall by given last 10 day's growth rate
+def GetIncDecNums(data, growthRate):
+	numOfIncrease = 0
+	numOfDecrease = 0
+	for i in range(11, len(data)):
+		value = data[i-1][4]/data[i-11][4]
+		if not sp.isnan(value) and value > growthRate:
+			currentGrowth = data[i][4]/data[i][1]
+			if currentGrowth > 1:
+				numOfIncrease += 1
+			elif currentGrowth < 1:
+				numOfDecrease += 1
+	return numOfIncrease, numOfDecrease
+
+# it will return 2 maps, the key is the growth rate of last 10 days.
+# Value of 1st result is numOfRaises, Value of 2nd is numOfFalls
+def ProcessData(data):
+	increaseResult = {}
+	decreaseResult = {}
+	data = data[::-1]
+	for i in range(0, 60):
+		growthRate = 1 + (i/20)
+		params = GetIncDecNums(data, 1 + (i/20))
+		increaseResult[growthRate] = params[0]
+		decreaseResult[growthRate] = params[1]
+	return increaseResult, decreaseResult
+
+#############################################################################
+
+# The key is the growth rate of last 10 days, the value is 
+# (num of Increase) / (num of Decrease)
+resultIncrease = {}
+resultDecrease = {}
+
+# It will iterate all the files
 for i in os.listdir(root):
     if os.path.isfile(os.path.join(root,i)):
-    	print("opening file : " + i)
-    	fileContent = sp.genfromtxt(os.path.join(root,i), delimiter=",")
-    	for d in fileContent:
-    		data.append(d);
+    	print("Processing : " + i)
+    	d = sp.genfromtxt(os.path.join(root,i), delimiter=",")
+    	dataResult = ProcessData(d)
+    	for key in dataResult[0]:
+    		if resultIncrease.has_key(key):
+    			resultIncrease[key] += dataResult[0][key]
+    		else:
+    			resultIncrease[key] = dataResult[0][key]
+    	for key in dataResult[1]:
+    		if resultDecrease.has_key(key):
+    			resultDecrease[key] += dataResult[1][key]
+    		else:
+    			resultDecrease[key] = dataResult[1][key]
+	print(resultIncrease)
+	print(resultDecrease)
 
-growth = []
+xs = sorted(resultIncrease.keys())
+ys = []
+for x in xs:
+	ys.append(resultIncrease[x]/resultDecrease[x])
 
-for d in data:
-	value = d[4]/d[1]
-	if not sp.isnan(value):
-		growth.append(value)
-
-def GetSubGrowth(grow, numOfInc):
-	sub = []
-	for i in range(numOfInc, len(grow)):
-		cond = True
-		for j in range(1,numOfInc + 1):
-			if grow[i-j] < 1:
-				cond = False;
-		if cond :
-			sub.append(growth[i])
-	return sub
-
-def GetIncProbAndDecProb(grow):
-	numOfInc = 0
-	numOfDec = 0
-	for g in grow:
-		if g > 1:
-			numOfInc += 1
-		elif g < 1:
-			numOfDec += 1
-	return numOfInc/len(grow), numOfDec/len(grow)
-
-increaseProb = []
-decreaseProb = []
-for i in range(1, 20):
-	print("Calculating sub of " + str(i))
-	sub = GetSubGrowth(growth, i)
-	param = GetIncProbAndDecProb(sub)
-	increaseProb.append(param[0])
-	decreaseProb.append(param[1])
-
-plt.grid()
-plt.plot(range(1, len(increaseProb) + 1), increaseProb)
-plt.plot(range(1, len(decreaseProb) + 1), decreaseProb)
+plt.plot(xs, ys)
 plt.show()
