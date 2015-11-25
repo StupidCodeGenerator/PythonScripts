c1e49caaaa27afd4ab05b1a35ac570e061583bfd
StupidCodeGenerator <c@thebgame.com>
2015-11-25 21:20:56 +0800

    update

--- StockAnalysis/TotalRaiseOfNDays_CurrentGrowth.py
+++ StockAnalysis/TotalRaiseOfNDays_CurrentGrowth.py
@@ -13,28 +13,35 @@ root = sys.argv[1]
 
 resultQuantity = {}
 result = {}
+
 # It will count how many raises before current step
-def TotalRaiseOfNDays(data, currentDay, n):
+def TotalRaiseBeforeNDays(data, currentDay, n):
 	result = 0
 	for i in range(currentDay - n, currentDay):
+		result += math.log(data[i][4]/data[i][1])
+	return result
+
+def TotalRaiseAfterNDays(data, currentDay, n):
+	result = 0
+	for i in range(currentDay, currentDay + n):
 		result += math.log(data[i][4]/data[i][1])
 	return result
 
 # It will fix the results above
 def ProcessData(data):
 	data = data[::-1]
-	n = 10
-	for i in range(n, len(data)):
-		currentGrowth = math.log(data[i][4] / data[i][1])
-		totalRaise = TotalRaiseOfNDays(data, i, n)
-		key = currentGrowth // 0.01 * 0.01
-		if not sp.isnan(key):
+	n = 20
+	for i in range(n, len(data) - n):
+		currentGrowth = TotalRaiseAfterNDays(data, i, n)
+		totalRaise = TotalRaiseBeforeNDays(data, i, n)
+		key = totalRaise
+		if not (sp.isnan(key) or key == 0):
 			if result.has_key(key):
 				resultQuantity[key] += 1
-				result[key] += totalRaise
+				result[key] += currentGrowth
 			else:
 				resultQuantity[key] = 1
-				result[key] = totalRaise
+				result[key] = currentGrowth
 
 ########################################################################
 
@@ -44,7 +51,6 @@ for i in os.listdir(root):
     	print("Processing : " + i)
     	data = sp.genfromtxt(os.path.join(root,i), delimiter=",")
     	ProcessData(data)
-
 xs = sorted(result.keys())
 ys = []
 ys2 = []
@@ -54,6 +60,7 @@ for x in xs:
 
 f, (ax1, ax2) = plt.subplots(2,1)
 ax1.plot(xs, ys)
-plt.title("TotalRaiseOfNDays and Data Density")
+plt.title("Total growth in next 10 days and Data Density")
+plt.xlabel("total raise in last 10 days")
 ax2.plot(xs, ys2)
 plt.show()
