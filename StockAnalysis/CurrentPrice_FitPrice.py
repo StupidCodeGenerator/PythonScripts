8c5d9478335185e1c7ba3e6e9a2ad88d8540e5b1
StupidCodeGenerator <c@thebgame.com>
2015-11-26 21:33:10 +0800

    update

--- StockAnalysis/CurrentPrice_FitPrice.py
+++ StockAnalysis/CurrentPrice_FitPrice.py
@@ -33,18 +33,24 @@ def MostProbPrice(data):
 			break; # if the plot goes down, stop searching.
 	return x[maxIndex]
 
+def TotalRaiseAfterNDays(data, currentDay, n):
+	result = 0
+	for i in range(currentDay, currentDay + n):
+		result += math.log(data[i][4]/data[i][1])
+	return result
 
 # It will fix the results above
 def ProcessData(data):
+	n = 10
 	growthOfThisData = 0
 	mostProbPrice = MostProbPrice(data)
 	if mostProbPrice == 0:
 		return
 	print("FitResult : " + str(mostProbPrice))	
-	for i in range(0, len(data)):
+	for i in range(0, len(data) - n):
 		if not (sp.isnan(data[i][1]) or sp.isnan(data[i][4])):
-			if data[i][1] > 0:
-				currentGrowth = math.log(data[i][4] / data[i][1])
+			if data[i][5] > 0:
+				currentGrowth = TotalRaiseAfterNDays(data, i, n)
 				pm = data[i][4] / mostProbPrice
 				key = (pm // 0.05) * 0.05
 				if result.has_key(key):
@@ -61,15 +67,18 @@ for i in os.listdir(root):
     if os.path.isfile(os.path.join(root,i)):
     	print("Processing : " + i)
     	data = sp.genfromtxt(os.path.join(root,i), delimiter=",")
-    	ProcessData(data)
+    	try:
+    		ProcessData(data)
+    	except:
+    		print("Fail file : " + i)
 
 resultX = []
 resultY = []
 for key in sorted(result):
 	resultX.append(key)
-	resultY.append(result[key] / resultQuantities[key])
+	resultY.append(math.exp(result[key] / resultQuantities[key]))
 plt.grid()
 plt.plot(resultX, resultY)
-plt.xlabel("CurrentPrice / FitPrice")
-plt.ylabel("Avr(ln(GrowthRate))")
+plt.xlabel("Exp(CurrentPrice / FitPrice)")
+plt.ylabel("E(GrowthRate)")
 plt.show()
