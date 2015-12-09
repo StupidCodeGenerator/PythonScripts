d1c00716b2941413f4aa8c488ad7c1b58ec8be13
StupidCodeGenerator <c@thebgame.com>
2015-12-09 22:50:27 +0800

    update

--- StockAnalysis/ExpectProfitResearch/DataCrawler.py
+++ StockAnalysis/ExpectProfitResearch/DataCrawler.py
@@ -1,45 +1,56 @@
-﻿from __future__ import division
+﻿# -- Data Structure --
+# base: code(key), lastDate, currentPrice
+# stock: code(key), open, high, low, close, volume, adjClose
+
+from __future__ import division
 import numpy as np
 import scipy as sp
 import urllib2
 import sys
 import os
+import datetime
 
-BASE_INDEX = {"LAST_DATE":0 , "CURRENT_PRICE":1}
-STOCK_DATA_INDEX = {"CODE":0, "OPEN":1, "HIGH":2, "LOW":3 "CLOSE":4 \
-					"VOLUME":5, "ADJ_CLOSE":6}
-
-# Load from directory/base.csv and store by rows.
-# Each row stores one stock info
-# Need to be loaded at the begining of the program starts.
-# The key is the stock code
-# The value will be a array of CurrentPrice and LastDate
-stockBase = {}
-
-# Load content in directory/base.csv and store into stockBase
+# Load content in directory/base.csv
 def LoadStockBase(directory):
+	stockBase = {}
 	print("LoadingBase...")
 	filePath = os.path.join(directory, "base.csv")
 	baseString = ""
-	with open(filePath, "r") as file:
-		baseString = file.read()
-		print(baseString)
+	try:
+		with open(filePath, "r") as file:
+			baseString = file.read()
+			print(baseString)
+	except:
+		os.mkdir(directory)
+		with open(filePath, "a") as file:
+			file.write("")
+			print("Create new base file")
 	if baseString:
-		stockBaseArray = sp.genfromtxt(filePath, delimiter=",")
-		for stockData in stockBaseArray:
-			key = stockData[0]
-			stockBase[key] = stockBaseArray[1:]
+		baseLines = baseString.split("\n")
+		for line in baseLines:
+			colums = line.split(",")
+			key = colums[0]
+			stockBase[key] = colums[1:]
+	return stockBase
 
 # It will update the stock info of the given key in stockBase.
-def UpdateStockBase(stockCode, lastDate, currentPrice):
+def UpdateStockBase(stockCode, lastDate, currentPrice, stockBase, directory):
+	stockArray = [lastDate, currentPrice]
+	stockBase[stockCode] = stockArray
+	filePath = os.path.join(directory, "base.csv")
+	print("Updating base : " + filePath)
+	with open(filePath, "w") as file:
+		for key in stockBase:
+			string = str(stockCode) +","+ str(lastDate) +","+ str(currentPrice)
+			file.write(string)
 
 # Download from url and return the result as a file pointer
-def DownloadStock(startDate, endDate, code, directory):
+def DownloadStock(startDate, endDate, code, directory, stockBase):
 	start_ymd = startDate.split("-")
 	end_ymd = endDate.split("-")
 	# the month should -1 because the yahoo api is like that ...
-	start_ymd[1] = str(int(start_ymd[1]) - 1)
-	end_ymd[1] = str(int(end_ymd[1]) - 1)
+	start_ymd[1] = str(int(start_ymd[1]) - 1).zfill(2)
+	end_ymd[1] = str(int(end_ymd[1]) - 1).zfill(2)
 	result = ""
 	print("Try sz")
 	try:
@@ -50,25 +61,49 @@ def DownloadStock(startDate, endDate, code, directory):
 		print("Downloading : " + str_url)
 		result = urllib2.urlopen(str_url).read()
 	except:
-		print("Try ss")
-		str_url = "http://table.finance.yahoo.com/table.csv?s="+code+\
-			".ss&d="+end_ymd[1]+"&e="+end_ymd[2]+"&f="+end_ymd[0]+\
-			"&g=d&a="+start_ymd[1]+"&b="+start_ymd[2]+"&c="+start_ymd[0]+\
-			"&ignore=.csv"
-		print("Downloading : " + str_url)
-		result = urllib2.urlopen(str_url).read()
+		print("Fail...")
+	if not result:
+		try:
+			print("Try ss")
+			str_url = "http://table.finance.yahoo.com/table.csv?s="+code+\
+				".ss&d="+end_ymd[1]+"&e="+end_ymd[2]+"&f="+end_ymd[0]+\
+				"&g=d&a="+start_ymd[1]+"&b="+start_ymd[2]+"&c="+start_ymd[0]+\
+				"&ignore=.csv"
+			print("Downloading : " + str_url)
+			result = urllib2.urlopen(str_url).read()
+		except:
+			print("Fail...")
 	if result:
 		# I don't know why [1:-1:-1] dosen't work
 		rows = result.split("\n")[1:-1]
 		rows = rows[::-1]
-		print(rows)
-		with open(str(code) + ".csv", "a") as file:
+		filePath = os.path.join(directory, str(code) + ".csv")
+		with open(filePath, "a") as file:
 			for row in rows:
 				file.write(row + "\n");
-		print("File write to : " + str(file))
-		currentPrice = rows[len(rows) - 1][6]
+		print("File write to : " + str(filePath))
+		lastRowData = rows[len(rows) - 1].split(",")
+		currentPrice = lastRowData[6]
+		lastDate = lastRowData[0]
+		print("LastRow : " + rows[len(rows) - 1])
+		UpdateStockBase(code, lastDate, currentPrice, stockBase, directory)
 
 # ---------------------------------------------------
 # START
 
-DownloadStock("2015-10-01", "2015-10-30", "000001", "./")
+# 1. Load the stock base and calculate today
+stockBase = LoadStockBase(sys.argv[1])
+today = str(datetime.date.today())
+print("Today : " + today)
+
+# 2. Try to download all stocks. before that, check if it exists in base
+#    If so, download missing dates
+for i in range(1, 999999):
+	stockCode = str(i).zfill(6)
+	if stockBase.has_key(stockCode):
+		lastDate = stockBase[stockCode][0]
+		print("lastDate" + lastDate)
+		DownloadStock(lastDate, today, stockCode, sys.argv[1], stockBase)
+	else:
+		print("New Stock : " + stockCode)
+		DownloadStock("1991-11-16", today, stockCode, sys.argv[1], stockBase)
